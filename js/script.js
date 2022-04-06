c6dc3709224916cd4bdbdb9f10a4b26636558d6c
carinalsilverio <carinalsilverio@gmail.com>
2022-04-06 19:15:48 -0300

    Início responsivo

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/*class MobileNavbar {
+    constructor(mobileMenu, navList,navLinks) {
+        this.mobileMenu = document.querySelectorAll(mobileMenu);
+        this.navList = document.querySelectorAll(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log("Olá"));        
+    }
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar (
+    ".menu-responsivo",
+    ".lista-nav",
+    ".lista-nav li",
+);
+
+mobileNavbar.init();
+
+*/
+
